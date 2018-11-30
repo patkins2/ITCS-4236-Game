71c1e103073cdb61671f5c2939b249f95a25ddee
Paul Atkins <patkins2@uncc.edu>
2018-11-29 23:40:36 -0500

    Field targets added
    Added field targets and a random number generator to assign where it will go.

--- 4236 Baseball/Assets/Scripts/BaseballScript.cs
+++ 4236 Baseball/Assets/Scripts/BaseballScript.cs
@@ -2,20 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class BaseballScript : MonoBehaviour {
 
     [SerializeField] private GameObject currentPlayer;
     [SerializeField] private GameObject rightHand;
     [SerializeField] private GameObject ball;
     [SerializeField] private GameObject strikeZone;
-    [SerializeField] private GameObject target;
     [SerializeField] private float throwForce = 5000f;      //How hard the ball is thrown
 
     private Rigidbody rb;
     private Collision collision;
     private Vector3 relativePos;
     private Quaternion rotation;
-   
+    private GameObject target;
+
 
 
     // Use this for initialization
@@ -50,11 +51,59 @@ public class BaseballScript : MonoBehaviour {
         if (other.gameObject.CompareTag("Bat"))
         {
             print("Ball hit bat");
+
+            //Random rnd = new Random();
+            int targetNum = Random.Range(1, 10); //random number between 1 and 9
+
+            //based on the random number, assign where the ball will go. 
+            switch (targetNum)
+            {
+                case 1:
+                    target = GameObject.Find("Target1");
+                    break;
+
+                case 2:
+                    target = GameObject.Find("Target2");
+                    break;
+
+                case 3:
+                    target = GameObject.Find("Target3");
+                    break;
+
+                case 4:
+                    target = GameObject.Find("Target4");
+                    break;
+
+                case 5:
+                    target = GameObject.Find("Target5");
+                    break;
+
+                case 6:
+                    target = GameObject.Find("Target6");
+                    break;
+
+                case 7:
+                    target = GameObject.Find("Target7");
+                    break;
+
+                case 8:
+                    target = GameObject.Find("Target8");
+                    break;
+
+                case 9:
+                    target = GameObject.Find("Target9");
+                    break;
+
+                default:
+                    break;
+            }
+
+
             this.GetComponent<Collider>().isTrigger = false;
             rb.useGravity = true;
 
              relativePos = target.transform.position - ball.transform.position;
-             rb.AddForce(relativePos.normalized * throwForce * 2);
+             rb.AddForce(relativePos.normalized * throwForce * 2); // need to look into ballistic velocity
             /*List<Vector3> positionList;
             // ...
 
