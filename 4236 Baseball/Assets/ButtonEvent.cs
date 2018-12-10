91b04107079de51129b8fa937b8b9264da6daf4c
ChrisBratti <42649073+ChrisBratti@users.noreply.github.com>
2018-12-09 19:54:13 -0500

    Added main menu
    Added main menu with buttons that navigate to the game or exit the game

---
+++ 4236 Baseball/Assets/ButtonEvent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonEvent : MonoBehaviour
+{
+    public void OnClick(int num)
+    {
+       
+        if (num == 1)
+        {
+            SceneManager.LoadScene("Game");
+        }
+        
+        else if (num == 3)
+        {
+            Application.Quit();
+        }
+    }
+}
